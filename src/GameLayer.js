01cd7595f604301110ee32396da530860e4553ff
Kyuuri <dark_e2102@hotmail.com>
2014-04-01 20:14:57 +0700

    Use array of explosion instead of 9 objects (clean)

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -91,85 +91,38 @@ var GameLayer = cc.LayerColor.extend({
     initPlayerExplosion: function(){
         var exScale = 2.0;
 
-        this.ex1 = new Explosion();
-        this.ex1.initWithFile( 'images/ex1.png' );
+        this.ex = new Array();
+        this.count = 0;
 
-        this.ex2 = new Explosion();
-        this.ex2.initWithFile( 'images/ex2.png' );
-
-        this.ex3 = new Explosion();
-        this.ex3.initWithFile( 'images/ex3.png' );
-
-        this.ex4 = new Explosion();
-        this.ex4.initWithFile( 'images/ex4.png' );
-
-        this.ex5 = new Explosion();
-        this.ex5.initWithFile( 'images/ex5.png' );
-
-        this.ex6 = new Explosion();
-        this.ex6.initWithFile( 'images/ex6.png' );
-
-        this.ex7 = new Explosion();
-        this.ex7.initWithFile( 'images/ex7.png' );
-
-        this.ex8 = new Explosion();
-        this.ex8.initWithFile( 'images/ex8.png' );
-
-        this.ex9 = new Explosion();
-        this.ex9.initWithFile( 'images/ex9.png' );
-
-        this.ex1.setScale(exScale);
-        this.ex2.setScale(exScale);
-        this.ex3.setScale(exScale);
-        this.ex4.setScale(exScale);
-        this.ex5.setScale(exScale);
-        this.ex6.setScale(exScale);
-        this.ex7.setScale(exScale);
-        this.ex8.setScale(exScale);
-        this.ex9.setScale(exScale);
-
+        for(var i = 1 ; i < 10 ; i++){
+            var src = 'images/ex' + i + '.png';
+            this.ex[i-1] = new Explosion();
+            this.ex[i-1].initWithFile( src );
+            this.ex[i-1].setScale(exScale);
+        }
 
     },
 
     explodePlayer: function(){
         var pos = this.player.getPosition();
 
+
         if(this.explosionCount == 0){
-            this.ex1.setPosition(pos);
-            this.addChild(this.ex1,3);
+            this.ex[0].setPosition(pos);
+            this.addChild(this.ex[0],3);
 
             //temp
             this.endLabel = cc.LabelTTF.create( 'กากงะ T^T', 'Arial', 100 );
             this.endLabel.setPosition( new cc.Point( 300, 400 ) );
             this.addChild(this.endLabel,3);
         }
-        else if(this.explosionCount == 3){
-            this.shiftExplodeFrame(this.ex1, this.ex2, pos);
-        }
-        else if(this.explosionCount == 6){
-            this.shiftExplodeFrame(this.ex2, this.ex3, pos);
-        }
-        else if(this.explosionCount == 9){
-            this.shiftExplodeFrame(this.ex3, this.ex4, pos);
-        }
-        else if(this.explosionCount == 12){
-            this.shiftExplodeFrame(this.ex4, this.ex5, pos);
-        }
-        else if(this.explosionCount == 15){
-            this.shiftExplodeFrame(this.ex5, this.ex6, pos);
-        }
-        else if(this.explosionCount == 18){
-            this.shiftExplodeFrame(this.ex6, this.ex7, pos);
-        }
-        else if(this.explosionCount == 21){
-            this.shiftExplodeFrame(this.ex7, this.ex8, pos);
-        }
-        else if(this.explosionCount == 24){
-            this.shiftExplodeFrame(this.ex8, this.ex9, pos);
-        }
         else if(this.explosionCount == 27){
-            this.removeChild(this.ex9);
+            this.removeChild(this.ex[8]);
             this.state = GameLayer.END;
+        }
+        else if(this.explosionCount % 3 == 0){
+            this.shiftExplodeFrame(this.ex[this.count], this.ex[this.count+1], pos);
+            this.count++;
         }
 
         this.explosionCount++;
