5f7ad507728a2bb584c2995e26ca1ef0ccbbf12c
Kyuuri <dark_e2102@hotmail.com>
2014-04-18 02:39:06 +0700

    Add missing file

---
+++ src/ObstacleCreator.js
@@ -0,0 +1,63 @@
+var ObstacleCreator = cc.Sprite.extend({
+    ctor: function( game ) {
+        this._super();
+        this.initWithFile( 'images/advancedObstacle.png' );
+        this.obstacles = [];
+
+        for(var i = 1 ; i <= 3 ; i++){
+            this.obstacles.push( new ObstacleTest() );
+        }
+
+        this.gameLayer = game;
+        this.scheduleUpdate();
+        this.schedule(this.shoot,3,3,3);
+    },
+
+    update: function(){
+        console.log(this.gameLayer.state);
+        if( this.gameLayer.state == GameLayer.STATES.DEAD || this.gameLayer.state == GameLayer.STATES.END ){
+            this.unschedule(this.shoot);
+        }
+        else if( this.isCollide() ){
+            this.gameLayer.state = GameLayer.STATES.DEAD;
+
+            this.gameLayer.updateGameLayer();
+
+            for(var i = 0 ; i < this.obstacles.length ; i++){
+                this.obstacles[i].stop();
+            }
+        }
+    },
+
+    isCollide: function(){
+
+        for(var i = 0 ; i < this.obstacles.length ; i++){
+            if(this.obstacles[i].closeTo( this.gameLayer.player )){
+                if(this.gameLayer.state == GameLayer.STATES.STARTED){
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+
+    //test shoot
+    shoot: function(){
+
+        for(var i = 0 ; i < 3 ; i++){
+            this.obstacles[i].vx = -2 + (i+1);
+            this.obstacles[i].vy = -8;
+            this.obstacles[i].setPosition( this.getPosition() );
+
+            this.gameLayer.addChild(this.obstacles[i], 10);
+            this.obstacles[i].start();
+            this.obstacles[i].scheduleUpdate();
+        }
+    },
+
+
+
+
+});
+
---
+++ src/ObstacleTest.js
@@ -0,0 +1,92 @@
+var ObstacleTest = cc.Sprite.extend({
+    ctor: function() {
+        this._super();
+        this.initWithFile( 'images/obstacle.png' );
+        this.setScale(Obstacle.SCALE);
+        this.vx = 0;
+        this.vy = -2 + Math.floor(Math.random() * Obstacle.GRAVITY);
+
+        this.isSlow = false;
+        this.slowRate = 0;
+
+        this.sizeX = Obstacle.INITIAL_SIZE_X;
+        this.sizeY = Obstacle.INITIAL_SIZE_Y;
+
+        this.started = false;
+    },
+
+    update: function(dt) {
+        if(!this.isSlow){
+            this.updateObstacle();
+        }
+        else{
+            if(this.slowRate % 3 == 0){
+                this.updateObstacle();
+            }
+        }
+        this.slowRate++;
+    },
+
+    updateObstacle: function(){
+        if(this.started){
+            var pos = this.getPosition();
+
+            if( this.isOutOfScreen() ){
+                this.destroySelf();
+            }
+            else{
+                this.setPosition( new cc.Point( pos.x + this.vx, pos.y + this.vy ) );
+            }
+            
+        }
+    },
+
+    activateSlow: function(isSlow){
+        this.isSlow = isSlow;
+    },
+
+    randomPosition: function() {
+        var posx = 1 + Math.floor(Math.random() * screenWidth);
+        var posy = 1 + Math.floor(Math.random() * screenHeight);
+        this.setPosition(new cc.Point(posx, screenHeight + posy + this.sizeY));
+        
+        this.resetValue();
+    },
+
+    resetValue: function(){
+        this.vy = -2 + Math.floor(Math.random() * Obstacle.GRAVITY);
+    },
+
+    destroySelf: function(){
+        this.removeFromParent();
+    },
+
+    isOutOfScreen: function(){
+        var pos = this.getPosition();
+        var outX = ( pos.x < -100 ) || ( pos.x > screenWidth + 100 );
+        var outY = ( pos.y < -100 ) || ( pos.y > screenHeight + 100 );
+
+        return outX && outY;
+    },
+
+    closeTo: function( obj ) {
+        var myPos = this.getPosition();
+        var oPos = obj.getPosition();
+
+        return ((Math.abs(myPos.x - oPos.x) <= this.sizeX * Obstacle.SCALE) &&
+         (Math.abs(myPos.y - oPos.y) <= this.sizeY * Obstacle.SCALE));
+    },
+
+    start: function(){
+        this.started = true;
+    },
+
+    stop: function(){
+        this.started = false;
+    },
+
+    getType: function(){
+        return 0;
+    }
+
+});
