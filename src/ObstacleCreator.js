18249d039f1e9bfbde0220e212db8f2a0cf5e82b
Kyuuri <dark_e2102@hotmail.com>
2014-04-23 00:25:38 +0700

    Finish lock on style

--- src/ObstacleCreator.js
+++ src/ObstacleCreator.js
@@ -10,6 +10,8 @@ var ObstacleCreator = cc.Sprite.extend({
         this.crossNum = 0;
         this.crossV = 0;
 
+        this.lockOnV = 0;
+
         for(var i = 1 ; i <= 20 ; i++){
             this.obstacles.push( new ObstacleTest() );
         }
@@ -18,7 +20,8 @@ var ObstacleCreator = cc.Sprite.extend({
         this.scheduleUpdate();
 
         //this.shootMultiArc( 10, 1, 5, 2, -8);
-        this.shootCross( 8, 1, 5, 2, 8);
+        //this.shootCross( 8, 1, 5, 2, 8);
+        this.shootLockOn(1,5,2,8)
     },
 
     update: function(){
@@ -73,7 +76,7 @@ var ObstacleCreator = cc.Sprite.extend({
     shootCross: function( crossNum, interval, repeat, delay, v){
 
         this.crossNum = crossNum;
-        this.crossV = v
+        this.crossV = v;
 
         this.schedule( this.cross, interval, repeat, delay); 
     },
@@ -93,6 +96,51 @@ var ObstacleCreator = cc.Sprite.extend({
             this.obstacles[i].start();
             this.obstacles[i].scheduleUpdate();
         }
+    },
+
+    shootLockOn: function( interval, repeat, delay, v){
+
+        this.lockOnV = v;
+
+        this.schedule( this.lockOn, interval, repeat, delay); 
+    },
+
+    lockOn: function(){
+
+        var pos = this.getPosition();
+        var playerPos = this.gameLayer.player.getPosition();
+
+        var diffX = playerPos.x - pos.x;
+        var diffY = playerPos.y - pos.y;
+
+        var angle;
+
+        if( diffX == 0 && diffY > 0 ){
+            angle = Math.PI / 2;
+        }
+        else if( diffX == 0 && diffY < 0 ){
+            angle = 3 * Math.PI / 2;
+        }
+        else if ( diffX == 0 && diffY == 0){
+            angle = 0;
+        }
+        else{
+
+            angle = Math.abs( Math.atan( diffY / diffX ) ); //Q1
+
+            if( diffX < 0 && diffY > 0 ) angle = Math.PI - angle; //Q2
+            else if( diffX < 0 && diffY < 0 ) angle = Math.PI + angle; //Q3
+            else if( diffX > 0 && diffY < 0 ) angle = -angle; //Q4
+
+        }
+
+        this.obstacles[0].vx = this.lockOnV * Math.cos( angle );
+        this.obstacles[0].vy = this.lockOnV * Math.sin( angle );
+        this.obstacles[0].setPosition( this.getPosition() );
+
+        this.gameLayer.addChild( this.obstacles[0], 10 );
+        this.obstacles[0].start();
+        this.obstacles[0].scheduleUpdate();
     },
 
 
